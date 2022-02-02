608fa1a008ddc89df72bf5387c6f5f9a260113b7
LathaN-github <LathaN-github>
2022-02-02 20:57:32 +0530

    first commit

---
+++ Basic Java/src/Basic/Add.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int a = 10;
+	    int b = 20;
+	    int result =a +b;
+	    System.out.println(result);
+
+	}
+
+}
