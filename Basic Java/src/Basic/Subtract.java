c4790dc13ce55f189b1e7f49666e53e7f60e5496
LathaN-github <LathaN-github>
2022-02-02 21:09:54 +0530

    2 nd commit

---
+++ Basic Java/src/Basic/Subtract.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Subtract {
+
+	public static void main(String[] args) {
+		int a=7;
+		int b=3;
+		int result=a-b;
+		System.out.println("subtracting two numbers:" + result);
+		
+
+	}
+
+}
